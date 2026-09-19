9e34b3e8143d7e9de7f5273a9f9676655e1d31cc
Eduardo <poczynek10@hotmail.com>
2026-09-19 10:56:04 -0300

    Refactor index route to include empty board

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+def tabuleiro_vazio():
+    return [" "] * 9
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template(
+        "index.html", 
+        tabuleiro=tabuleiro_vazio() )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
-
